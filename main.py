8a65b93fa281f6a79078ec552c83377e3e1a71e4
patelnirav1706 <patelnirav1706@gmail.com>
2026-08-11 22:06:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+my_current_age = 18
+after_5_years_i_will_be = my_current_age + 5
+after_10_years_i_will_be_ = my_current_age + 10
+
+print("my current age is",my_current_age)
+print("after 5 years i will be",after_5_years_i_will_be)
+print("after 10 years i will be", after_10_years_i_will_be_)
+
+
